03c62aefd6105c3c2d6efa79bcaf04df55ec2d44
Bryan <bryan.boyko@gmail.com>
2015-07-12 19:41:35 -0700

    Initial Commit

---
+++ BBDangleCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBDangleCollectionView
+//
+//  Created by Bryan Boyko on 7/12/15.
+//  Copyright (c) 2015 BryanBoyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBDangleCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBDangleCollectionView
+//
+//  Created by Bryan Boyko on 7/12/15.
+//  Copyright (c) 2015 BryanBoyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
